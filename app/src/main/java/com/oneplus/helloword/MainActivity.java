d7723ac5d86f89247086fe92831bdd55b6185606
zhouhongmin <13671031830@163.com>
2016-10-10 14:21:56 +0800

    这是第一次更新

--- app/src/main/java/com/oneplus/helloword/MainActivity.java
+++ app/src/main/java/com/oneplus/helloword/MainActivity.java
@@ -2,6 +2,7 @@ package com.oneplus.helloword;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //这是一行注释注释注释，哈哈哈哈哈哈哈哈
+        ImageView imageView = new ImageView(this);
     }
 }
